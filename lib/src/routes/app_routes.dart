2ccdc91deff3476aff967aae528b91d775d1be78
Leticia Zalasik <leticiazalasik@gmail.com>
2026-04-07 13:30:47 -0300

    Retirada de rotas, dependências que não eram utilizadas

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -1,5 +1,4 @@
 class AppRoutes {
     static const String home = '/home';
     static const String history = '/history';
-    static const String details = '/details';
 }
--- lib/src/shared/storage/hive_config.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hive_flutter/hive_flutter.dart';
-
-class HiveConfig {
-  static Future<void> init() async {
-    await Hive.initFlutter();
-  }
-}
